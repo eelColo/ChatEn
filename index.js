46b3708bbe60170bccdb444c91cb601c3589a4b8
Franco <francolens2002@gmail.com>
2024-08-27 23:56:01 -0300

    Modificando estilo, añadiendo animaciones y contador de usuarios online

--- index.js
+++ index.js
@@ -5,32 +5,42 @@ import {createServer} from "http";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const ws = new Server(server);
 const PORT = process.env.PORT ?? 4500
-
+let usuariosOnline = 0;
 
 
 app.use(express.static("public"));
 
-//Conexion
-io.on("connection", (data) => {
-    console.log("Nueva conexion establecida", data)
+//Cuando hay una conexion
+ws.on("connection", (data) => {
+    console.log("Nueva conexion establecida: ")
+    usuariosOnline += 1
+    console.log("Usuarios online: " + usuariosOnline)
+
+    //Al haber una conexion emitimos un updateUserCount y enviamos la cantidad de usuarios
+    ws.emit("updateUserCount", usuariosOnline);
 
     //Eventos
     // Cuando el server recibe un mensaje
     data.on("message", (data) =>{
+        console.log("Mensaje recibido: ");
         console.log(data);
         // Envia un mensaje a los demas conectados 
-        io.emit("message", data)
+        ws.emit("message", data)
     })
-    
+    data.on("disconnect", (data) => {
+
+        ws.emit("updateUserCount", usuariosOnline)
+        console.log("Cliente desconectado")
+        usuariosOnline -= 1;
+        console.log("Usuarios totales: " + usuariosOnline)
+        console.log(data)
+    })
 })
 
 //Desconexion
-io.on("disconnect", (data) => {
-    console.log("Cliente desconectado")
-    console.log(data)
-})
+
 
 
 //Levantando servidor
--- public/scripts.js
+++ public/scripts.js
@@ -2,6 +2,15 @@ document.addEventListener('DOMContentLoaded', () =>{
     const socket = io(); //websocket
     const form = document.getElementById('entrada');
     const list = document.querySelector(".messages");
+    const online = document.getElementById("onlinewachos")
+
+    // Scroll automatico
+    function scrollear(){
+        console.log("hola")
+        list.scrollTop = list.scrollHeight;
+        console.log("hola2")
+    }
+
 
     //Recepcion de datos
     //Al recibir mensaje del servidor desestructura los datos en usuario y mensaje
@@ -10,8 +19,16 @@ document.addEventListener('DOMContentLoaded', () =>{
         const li = document.createElement('li');
         // Inserta dentro del Li el usuario y el mensaje recibido
         li.innerHTML = `<p><Strong>${user}> </Strong>${message}</p>`
+
         list.appendChild(li);
+        
     })
+
+    socket.on("updateUserCount", (numeroDeUsuarios) => {
+        
+        online.innerHTML = numeroDeUsuarios
+    })
+
 
 
     //Manejo de envio
@@ -28,10 +45,11 @@ document.addEventListener('DOMContentLoaded', () =>{
         // Se resetea el mensaje y se hace focus para que siga escribiendo
         form.message.value = "";
         form.message.focus()
-        form.user.value(e.target.user.value);
+        form.user.value = e.target.user.value;
+        scrollear()
     })
 
-
+    
 
 
 
