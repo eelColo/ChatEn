0579287a8d859e91b48b058ed03d54b156569df4
Franco <francolens2002@gmail.com>
2024-09-02 00:21:24 -0300

    Modificando input de usuario

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { json } from "express";
 import {Server} from "socket.io";
 import {createServer} from "http";
 
@@ -7,31 +7,45 @@ const app = express();
 const server = createServer(app);
 const ws = new Server(server);
 const PORT = process.env.PORT ?? 4500
+const HOST = process.env.HOST ?? "localhost"
 let usuariosOnline = 0;
-
+let mensjs = []
 
 app.use(express.static("public"));
 
 //Cuando hay una conexion
 ws.on("connection", (data) => {
-    console.log("Nueva conexion establecida: ")
+    
+    
+
+    
+    console.log("Nueva conexion establecida")
     usuariosOnline += 1
     console.log("Usuarios online: " + usuariosOnline)
-
+    
     //Al haber una conexion emitimos un updateUserCount y enviamos la cantidad de usuarios
-    ws.emit("updateUserCount", usuariosOnline);
-
+    
+    ws.emit("newUserCount", usuariosOnline);
+    
     //Eventos
     // Cuando el server recibe un mensaje
+    
+    
     data.on("message", (data) =>{
         console.log("Mensaje recibido: ");
         console.log(data);
+        mensjs += JSON.stringify(data)
+        console.log(mensjs)
         // Envia un mensaje a los demas conectados 
         ws.emit("message", data)
     })
+
+    
+    
+
     data.on("disconnect", (data) => {
 
-        ws.emit("updateUserCount", usuariosOnline)
+        ws.emit("newUserCount", usuariosOnline)
         console.log("Cliente desconectado")
         usuariosOnline -= 1;
         console.log("Usuarios totales: " + usuariosOnline)
@@ -44,5 +58,5 @@ ws.on("connection", (data) => {
 
 
 //Levantando servidor
-server.listen(PORT, console.log("---Servidor corriendo--- en: " + PORT))
+server.listen(PORT, HOST, console.log(`---Servidor corriendo--- en: ${HOST}:${PORT}`))
 
--- public/scripts.js
+++ public/scripts.js
@@ -1,44 +1,59 @@
+
 document.addEventListener('DOMContentLoaded', () =>{
     const socket = io(); //websocket
     const form = document.getElementById('entrada');
     const list = document.querySelector(".messages");
     const online = document.getElementById("onlinewachos")
-
-    // Scroll automatico
-    function scrollear(){
-        console.log("hola")
-        list.scrollTo(0, list.scrollHeight);
-        console.log(list.scrollHeight)
-    }
-
+    const displayuser = document.getElementById("userd")
+    //Obtener usuario
+    let username = prompt("Dame tu user")
+    displayuser.textContent = username + " > ";
+    console.log(username)
 
     //Recepcion de datos
     //Al recibir mensaje del servidor desestructura los datos en usuario y mensaje
     socket.on("message", ({user,message}) => {
         // Crea un List Item
         const li = document.createElement('li');
-        // Inserta dentro del Li el usuario y el mensaje recibido
-        li.innerHTML = `<p><Strong>${user}> </Strong>${message}</p>`
+        li.className = "d-flex gap-1"
+        const par0 = document.createElement('p')
+        const par1 = document.createElement('p')
+
+
+        // Inserta dentro del Li dos Parrafos que contienen el usuario y el mensaje con estilos especificos
+        
+        par0.textContent= user;
+        par0.innerText += " > "
+        par0.className = "user-title"
+
+        par1.textContent = message;
+
+        li.appendChild(par0);
+        li.appendChild(par1);
 
         list.appendChild(li);
         li.scrollIntoView({behavior:"smooth",block:"end"})
         
     })
-
-    socket.on("updateUserCount", (numeroDeUsuarios) => {
+    socket.on("loadmessages", (mensjs) =>{
         
-        online.innerHTML = numeroDeUsuarios
     })
 
+    socket.on("newUserCount", (numeroDeUsuarios) => {
+        
+        online.innerHTML = numeroDeUsuarios
+        
+    })
+    
 
 
     //Manejo de envio
     form.addEventListener("submit", (e) => {
         //Evito que el formulario me recargue la pagina
         e.preventDefault();
-
+        
         const data = {
-            user: e.target.user.value,
+            user: username,
             message: e.target.message.value
         }
         // Envio de datos al servidor
@@ -46,7 +61,7 @@ document.addEventListener('DOMContentLoaded', () =>{
         // Se resetea el mensaje y se hace focus para que siga escribiendo
         form.message.value = "";
         
-        form.user.value = e.target.user.value;
+        // form.user.value = e.target.user.value;
         li.scrollIntoView({behavior:"smooth",block:"end"})
     })
 
