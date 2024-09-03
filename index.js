eb499509d757ef6bea52ebcb236ecb2af334b8e9
Franco <francolens2002@gmail.com>
2024-09-02 21:41:07 -0300

    sacando llamada de la var host xd

--- index.js
+++ index.js
@@ -58,5 +58,5 @@ ws.on("connection", (data) => {
 
 
 //Levantando servidor
-server.listen(PORT, console.log(`---Servidor corriendo--- en: ${HOST}:${PORT}`))
+server.listen(PORT, console.log(`---Servidor corriendo--- en: ${HOST}`))
 
